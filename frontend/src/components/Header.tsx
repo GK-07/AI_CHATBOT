b7a48e890c931ea17c0b920e98f9742eefd9cd1c
Gaurav Galiyawala <galiyawalagaurav@gmail.com>
2023-11-02 01:17:57 +0530

    Buttons are added in header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,10 @@
 import { AppBar, Toolbar } from "@mui/material";
 import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
+  const auth = useAuth();
   return (
     <AppBar
       position="static"
@@ -9,6 +12,40 @@ const Header = () => {
     >
       <Toolbar sx={{ display: "flex" }}>
         <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go to Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                to="/"
+                text="Logout"
+                textColor="white"
+                onClick={auth?.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                to="/signup"
+                text="Signup"
+                textColor="white"
+              />
+            </>
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
